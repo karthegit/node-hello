c875ec5ad489e6f11327e3d51f799ea496304e7a
karthegit <47658500+karthegit@users.noreply.github.com>
2019-06-21 13:56:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = process.env.HOST || 'localhost';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
